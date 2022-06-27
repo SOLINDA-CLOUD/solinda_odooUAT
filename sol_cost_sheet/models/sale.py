576d527474d31c5cb524fa59959e6b2faffd711c
Siwi Wiyono <wiyonosiwi@gmail.com>
2022-06-27 22:56:07 +0700

    update payment schedule

--- sol_cost_sheet/models/sale.py
+++ sol_cost_sheet/models/sale.py
@@ -4,4 +4,5 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
     rab_id = fields.Many2one('cost.sheet',related='opportunity_id.rab_id', string='RAB', store=True)
+    final_profit = fields.Float(related='rab_id.final_profit_percent',store=True)
     
