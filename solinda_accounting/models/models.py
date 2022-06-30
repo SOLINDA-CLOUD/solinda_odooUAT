afe656007623639262fa1bc823690b9ae1e68e10
hamsky13 <ilham.ius13@gmail.com>
2022-06-30 15:09:06 +0700

    field reference tidak bisa sama

---
+++ solinda_accounting/models/models.py
@@ -0,0 +1,13 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.constrains('ref')
+    def _check_mobile_unique(self):
+        ref_counts = self.search_count(
+            [('ref', '=', self.ref), ('id', '!=', self.id)])
+        print(ref_counts)
+        if ref_counts > 0:
+            raise ValidationError("Reference number already exists!")
