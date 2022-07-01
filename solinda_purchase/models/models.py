517156d98332b7211966eff414b82e3574c9f2d4
inicoding <irfannurrahmat@outlook.com>
2022-07-01 09:49:20 +0700

    updt purchase kolom

--- solinda_purchase/models/models.py
+++ solinda_purchase/models/models.py
@@ -6,13 +6,18 @@ from odoo import models, fields, api
 class PurchaseRequisition(models.Model):
     _inherit = 'purchase.requisition'
 
+class DeliveryLocation(models.Model):
+    _name = 'delivery.location'
+    _description = 'Delivery Location'
+
+    name = fields.Char(string='Delivery Location')
 
 class PurchaseRequisitionLine(models.Model):
     _inherit = 'purchase.requisition.line'
 
-    merk_recommended = fields.Many2one('res.partner', string='Merk Recommended')
+    merk_recommended = fields.Char(string='Merk Recommended')
     price_target = fields.Float('Price Target')
     date_plan_required = fields.Date('Date Plan Required')
-    delivery_location = fields.Many2one('res.partner', string='Delivery Location')
+    delivery_location_id = fields.Many2one(string='Delivery Location', comodel_name='delivery.location', ondelete='restrict')
 
 
