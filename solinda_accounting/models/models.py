a369d8b9659b243e3bd8c53869ba255780194b43
hamsky13 <ilham.ius13@gmail.com>
2022-07-04 13:00:44 +0700

    test bill reference unique

--- solinda_accounting/models/models.py
+++ solinda_accounting/models/models.py
@@ -4,10 +4,13 @@ from odoo.exceptions import ValidationError
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    # @api.constrains('ref')
-    # def _check_mobile_unique(self):
-    #     ref_counts = self.search_count(
-    #         [('ref', '=', self.ref), ('id', '!=', self.id)])
-    #     print(ref_counts)
-    #     if ref_counts > 0:
-    #         raise ValidationError("Reference number already exists!")
+    @api.constrains('ref')
+    def _check_mobile_unique(self):
+        if self.ref:
+            ref_counts = self.search_count(
+                [('ref', '=', self.ref), ('id', '!=', self.id)])
+            print(self.ref)
+            if ref_counts > 0:
+                raise ValidationError("Reference number already exists!")
+        else:
+            return
