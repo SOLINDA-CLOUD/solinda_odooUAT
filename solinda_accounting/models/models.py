5976949ca0400269bfe6a2be361fef8442e86a76
hamsky13 <ilham.ius13@gmail.com>
2022-07-04 19:14:41 +0700

    qty received pada list vendor bill

--- solinda_accounting/models/models.py
+++ solinda_accounting/models/models.py
@@ -14,3 +14,25 @@ class AccountMove(models.Model):
                 raise ValidationError("Reference number already exists!")
         else:
             return
+
+    @api.depends('invoice_line_ids.qty_received_account')
+    def _compute_total_qty_received(self):
+        for this in self:
+            this.total_qty_received = sum(
+                this.invoice_line_ids.mapped('qty_received_account'))
+
+    total_qty_received = fields.Integer(
+        compute='_compute_total_qty_received', string='Qty Received', store=True)
+    invoice_line_ids = fields.One2many('account.move.line', 'move_id', string='Invoice lines',
+                                       copy=False, readonly=True,
+                                       domain=[
+                                           ('exclude_from_invoice_tab', '=', False)],
+                                       states={'draft': [('readonly', False)]})
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    purchase_line_id = fields.Many2one(
+        'purchase.order.line', 'Purchase Order Line', ondelete='set null', index=True)
+    qty_received_account = fields.Float(related='purchase_line_id.qty_received', store=True, string="Qty Received")
