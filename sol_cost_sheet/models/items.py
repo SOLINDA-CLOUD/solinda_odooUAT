68b569af277cf17ef475b90695254a453a9cdaba
Luthfi Achmaddin Nizar <Luthfinizar97@gmail.com>
2022-07-11 15:04:47 +0700

    add more condition in existng price

--- sol_cost_sheet/models/items.py
+++ sol_cost_sheet/models/items.py
@@ -69,9 +69,12 @@ class Item(models.Model):
                 stock_valuation = this.product_id.stock_valuation_layer_ids.sorted(reverse=True)
             else:
                 stock_valuation = this.env['purchase.order.line'].search([('product_id', '=', this.product_id.id),('po_confirm_date', '<=', fields.Date.context_today(this))],order="po_confirm_date desc",limit=1)
-            
-            cost = stock_valuation[0].price_unit if stock_valuation else 0.0
-            this.existing_price = cost
+            if stock_valuation:
+                cost = stock_valuation[0].unit_cost if stock_valuation else 0.0
+                this.existing_price = cost
+            else:
+                cost = 0
+                this.existing_price = cost
             if not this.rfq_price:
                 this.rfq_price = cost
             else:
